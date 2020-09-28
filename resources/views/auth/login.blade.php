8e18b0441e402746be718920297bfa230875c0a4
JaanbaazAkhtar <jaanbaazakhtar786@gmail.com>
2020-09-28 20:54:07 +0530

    LOGIN PAGE

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,181 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <div class="container-fluid justify-content-center">
+            <div class = "row " style="align-items:center; margin-top: 5%">
+
+
+                <div class="col-3"></div>
+
+                <div class="col-3 d-flex">
+
+                    <div class="bgi" >
+
+
+
+                        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel" style="width: 300px; height:400px">
+                            <div class="carousel-inner">
+                              <div class="carousel-item active">
+                                  <img src="Instaclone/carousel1.jpg" class="d-block" alt="..." width="205px" height="360px">
+                              </div>
+                              <div class="carousel-item">
+                                <img src="Instaclone/carousel2.jpg" class="d-block" alt="..." width="205px" height="360px">
+                              </div>
+                              <div class="carousel-item">
+                                <img src="Instaclone/carousel3.jpg" class="d-block" alt="..." width="205px" height="360px">
+                              </div>
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                          </div>
+                    </div>
+                    
                 </div>
+                <div class="col-4">
+                    <div class="card justiy-content-center m-3" style="width: 85%; border-radius:0px">
+                        <div  class="justify-content-center" style="margin:11%; align-items:center">
+
+
+                            <div class="d-flex" style="justify-content:center">
+                                <img  src="Instaclone/insta_title.png" width ="60%" height="3%">
+                            </div>
+
+                            <div  style=" padding-top:30px; padding-left:20px; padding-right:10px; padding-bottom:5px">
+                                <form method="POST" action="{{ route('login') }}">
+                                    @csrf
+            
+                                    <div class="form-group row">
+                                        
+                                            <input id="text" type="text" class="form-control @error('text') is-invalid @enderror" name="text" placeholder="Phone number, username, or email" value="{{ old('text') }}" required autocomplete="email" autofocus>
+            
+                                            @error('text')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        
+                                    </div>
+            
+                                    <div class="form-group row">
+                                      
+                                        
+                                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Password" required autocomplete="current-password">
+            
+                                            @error('password')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        
+                                    </div>
+            
+                                    <div class="form-group row mb-0 d-flex justify-content-center" >
+                                            <button type="submit" class="btn btn-primary" style="width: 100%; height:36px">
+                                                {{ __('Log In') }}
+                                            </button>
+                                    </div>
+                                </form>
+                                <div class="d-flex" style="margin-top: 20px; margin-left:-10px">
+                                    <div >
+                                        <img src="Instaclone/line.png" >
+                                    </div>
+
+                                   <div style="margin-left: 6%; margin-right:6%">OR</div>
+                                    
+                                    <div >
+                                        <img src="Instaclone/line.png">
+                                    </div>
+                                </div>
+
+                                <div class="justify-content-center d-flex" style="margin-top: 7%">
+                                    <a href="https://www.facebook.com/" style="color:#00376b;">
+                                        <img src="Instaclone/fb.png" width="25px" height="25px">
+                                        <b>Log in with Facebook</b>
+                                    </a>
+                                </div>
+                                <div class="d-flex justify-content-center" style="margin-top: 6%; margin-bottom:-8%">Forgot password?</div>
+                                
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                    <div class="card m-3 " style="align-items:center; height: 10%; width: 85%; border-radius:0px">
+                        <div class="d-flex justify-content-center" style="padding: 5%">
+                            Don't have an account?
+                            <a href="register"><b>Sign Up</b></a>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-center">Get the app.</div>
+                    <div class="d-flex justify-content-center" style="margin-top: 3%">
+                        <img src="Instaclone/appstore.png" alt="" widht="50px" height="40px" style="margin-right: 5px">
+                        <img src="Instaclone/googleplay.png" alt=""  widht="50px" height="40px" style="margin-left: 5px">
+                    </div>
+                </div>
+                <div class="col-2"></div>
+            </div>
+
+        </div>
+
+        <div class="container bottom-text" style="margin-top:5%; margin-bottom: 2%">
+            <div class="d-flex" style="align-items:center">
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">ABOUT</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">HELP</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">PRESS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">API</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">JOBS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">PROVACY</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">TERMS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">LOCATION</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">TOP ACCOUNTS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">HASHTAGS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">LANGUAGE</a>
+                    </li>
+                </ul>
+
+                <div style="margin-left:20%; color:grey">INSTAGRAM FROM FACEBOOK</div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+</body>
+</html>
